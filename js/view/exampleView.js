6589edf0df91ea6c59eb7a92f319bcac19ded314
josselin-vallee <josselin.vallee@epfl.ch>
2015-02-13 12:47:36 +0100

    Continued javascript some stuff somehow doesn't work

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -38,6 +38,44 @@ var ExampleView = function (container,model) {
 	 this.typeDropDown = container.find("#typeDropDown");
  }
  
- var DishSpecs = function(container, model){
+ var DishSpecs = function(container, model, id){
+	 this.dishName = container.find("#dishName");
+	 this.dishImg = container.find("#dishImg");
+	 this.dishDescr = container.find("#dishDescr");
+	 this.numberOfGuests = container.find("#numberOfGuests");
+	 this.ingredientTable = container.find("#ingredientTable");
+	 this.totalCost = container.find("#totalCost");
 	 
+	 this.dishName.html(model.getDish(id).name);
+	 this.dishImg.html(model.getDish(id).image);
+	 this.dishDescr.html(model.getDish(id).description);
+	 this.numberOfGuests.html(model.getNumberOfGuests());
+	 
+	 for(i=0; i < model.getDish(id).ingredients.length ; i++){
+		 var row = ingredientTable.insertRow(-1);
+		 var cell0 = row.insertCell(0);
+		 var cell1 = row.insertCell(1);
+		 var cell2 = row.insertCell(2);
+		 
+		 cell0.innerHTML = model.getDish(id).ingredients[i].quantity;
+		 cell1.innerHTML = model.getDish(id).ingredients[i].name;
+		 cell2.innerHTML = model.getDish(id).ingredients[i].price;
+	 }
+	 
+	 this.totalCost.html(model.getDishCost(2));
+ }
+ 
+ var DishOverview = function(container, model){
+	 this.starterimg = container.find("#starterimg");
+	 this.startername = container.find("#startername");
+	 this.startercost = container.find("#startercost");
+	 this.mainimg = container.find("#mainimg");
+	 this.mainname = container.find("#mainname");
+	 this.maincost = container.find("#maincost");
+	 this.dessertimg = container.find("#dessertimg");
+	 this.dessertname = container.find("#dessertname");
+	 this.dessertcost = container.find("#dessertcost");
+	 
+	 this.startername.html(model.getFullMenu()[0].name);
+	
  }
